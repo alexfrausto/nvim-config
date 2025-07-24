3de4981e5ecf88685f19a526845d58dc50c2ad44
Alejandro Frausto <inge.alexfrausto@gmail.com>
2025-07-24 14:44:09 -0600

    Change dadbod for dbee

--- init.lua
+++ init.lua
@@ -743,6 +743,19 @@ require('lazy').setup {
     dependencies = {
       'rcarriga/cmp-dap',
       { 'roobert/tailwindcss-colorizer-cmp.nvim', config = true },
+      {
+        'MattiasMTS/cmp-dbee',
+        dependencies = {
+          { 'kndndrj/nvim-dbee' },
+        },
+        ft = 'sql',
+        opts = {},
+      },
+    },
+    opts = {
+      sources = {
+        { 'cmp-dbee' },
+      },
     },
   },
   {
@@ -1164,46 +1177,42 @@ require('lazy').setup {
     ft = { 'markdown', 'norg', 'rmd', 'org', 'codecompanion' },
   },
   {
-    'tpope/vim-dadbod',
-    cmd = 'DB',
-  },
-  {
-    'kristijanhusak/vim-dadbod-completion',
-    dependencies = 'vim-dadbod',
-    init = function()
-      vim.api.nvim_create_autocmd('FileType', {
-        callback = function()
-          local cmp = require 'cmp'
-          local sources = vim.tbl_map(function(source)
-            return { name = source.name }
-          end, cmp.get_config().sources)
-
-          table.insert(sources, { name = 'vim-dadbod-completion' })
-
-          cmp.setup.buffer { sources = sources }
-        end,
-      })
-    end,
-  },
-  {
-    'kristijanhusak/vim-dadbod-ui',
-    cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer' },
-    dependencies = 'vim-dadbod',
-    keys = {
-      { '<leader>D', '<cmd>DBUIToggle<CR>', desc = 'Toggle DBUI' },
+    'kndndrj/nvim-dbee',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
     },
-    init = function()
-      local data_path = vim.fn.stdpath 'data'
+    build = function()
+      require('dbee').install()
+    end,
+    config = function()
+      require('dbee').setup {
+        result = {
+          mappings = {
+            -- yank rows as csv/json
+            { key = '<leader>yj', mode = 'n', action = 'yank_current_json' },
+            { key = '<leader>yj', mode = 'v', action = 'yank_selection_json' },
+            { key = '<leader>yJ', mode = '', action = 'yank_all_json' },
+            { key = '<leader>yc', mode = 'n', action = 'yank_current_csv' },
+            { key = '<leader>yc', mode = 'v', action = 'yank_selection_csv' },
+            { key = '<leader>yC', mode = '', action = 'yank_all_csv' },
+          },
+        },
+        editor = {
+          -- mappings for the buffer
+          mappings = {
+            -- run what's currently selected on the active connection
+            { key = '<leader>S', mode = 'v', action = 'run_selection' },
+            -- run the whole file on the active connection
+            { key = '<leader>S', mode = 'n', action = 'run_file' },
+            -- run what's under the cursor to the next newline
+            { key = '<CR>', mode = 'n', action = 'run_under_cursor' },
+          },
+        },
+      }
 
-      vim.g.db_ui_auto_execute_table_helpers = 1
-      vim.g.db_ui_save_location = data_path .. '/dadbod_ui'
-      vim.g.db_ui_show_database_icon = true
-      vim.g.db_ui_tmp_query_location = data_path .. '/dadbod_ui/tmp'
-      vim.g.db_ui_use_nerd_fonts = true
-      vim.g.db_ui_use_nvim_notify = false
-      vim.g.db_ui_win_position = 'right'
-
-      vim.g.db_ui_execute_on_save = false
+      vim.keymap.set('n', '<leader>D', function()
+        require('dbee').open()
+      end, { desc = '[D]atabase Explorer' })
     end,
   },
   {
